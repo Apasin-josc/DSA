25ea6a322919c8eeebeab5cdb15eff0f15a74511
omarscoppola <joseomar_coppola@hotmail.com>
2024-11-06 15:58:34 +0100

    MAIN-adding a linkedlist from scratch

---
+++ LinkedList/LinkedListFromScratch.js
@@ -0,0 +1,38 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+console.log(`hello there 😉`);
+myLinkedList.append(16);
+myLinkedList.prepend(53);
+console.log(myLinkedList);
