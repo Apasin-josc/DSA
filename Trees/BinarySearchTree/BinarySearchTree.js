42ee1518b1251aa9f265e70e923afa457b8209fb
omarscoppola <joseomar_coppola@hotmail.com>
2024-11-13 12:24:42 +0100

    MAIN-adding a binarysearchtree implementation from scratch - moving there

---
+++ Trees/BinarySearchTree/BinarySearchTree.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    }
+    let currentNode = this.root;
+  }
+  lookup(value) {}
+}
+
+const tree = new BinarySearchTree();
+JSON.stringify(traverse(tree.root));
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
+//      9
+//  4       20
+//1   6   15  170
